07c4c87e39d5817dcd107689386f49cb3e31addb
Loli Hernandez <mhernandezv30@gmail.com>
2020-03-01 22:30:56 -0600

    media terminada la tabla clase_dias

---
+++ database/migrations/2020_03_02_042115_create_clase_dias_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClaseDiasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clase_dias', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('clase_id');
+            $table->unsignedBigInteger('dia_id');
+            $table->foreign('clase_id')->references('id')->on('dias');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clase_dias');
+    }
+}
