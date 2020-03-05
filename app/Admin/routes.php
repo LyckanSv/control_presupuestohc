25da1223bb89d250b58f5f451075add915951dcb
Loli Hernandez <mhernandezv30@gmail.com>
2020-03-04 21:33:11 -0600

    Agrego apis a la rutas

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -19,16 +19,41 @@ Route::group([
     $router->get('/api/aulas', 'AulaController@aulas');
     
     $router->resource('dias', DiaController::class);
+    $router->get('/api/dias', 'DiaController@dias');
+
     $router->resource('presupuestos', PresupuestoController::class);
+    $router->get('/api/presupuestos', 'PresupuestoController@presupuestos');
+    
     $router->resource('escuelas', EscuelaController::class);
+    $router->get('/api/escuelas', 'EscuelaController@escuelas');
+
     $router->resource('clase-impartidas', ClaseImpartidaController::class);
+    $router->get('/api/clase-impartidas', 'ClaseImpartidaController@clasesImpartidas');    
+
     $router->resource('tipo-acreditaciones', TipoAcreditacionController::class);
+    $router->get('/api/tipo-acreditaciones', 'AulaController@tipoAcreditaciones');
+
     $router->resource('directores', DirectorController::class);
+    $router->get('/api/directores', 'DirectorController@directores');
+
     $router->resource('usuario-acreditacions', UsuarioAcreditacionController::class);
+    $router->get('/api/usuario-acreditaciones', 'UsuarioAcreditacionController@usuarioAcreditaciones');
+
     $router->resource('bloque-horas', BloqueHoraController::class);
+    $router->get('/api/bloque-horas', 'BloqueHoraController@bloqueHoras');
+
     $router->resource('estado-clases', EstadoClaseController::class);
+    $router->get('/api/estado-clases', 'EstadoClaseController@estadoClases');
+
     $router->resource('tipo-usuarios', TipoUsuarioController::class);
+    $router->get('/api/tipo-usuarios', 'TipoUsuarioController@tipoUsuarios');
+
     $router->resource('facultads', FacultadController::class);
+    $router->get('/api/facultades', 'FacultadController@facultades');
+    
     $router->resource('materias', MateriaController::class);
+    $router->get('/api/materias', 'MateriaController@materias');
+
     $router->resource('clases', ClaseController::class);
+    $router->get('/api/clases', 'ClaseController@clases');
 });
